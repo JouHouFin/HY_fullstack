42fd19d96d0aca843dbf50558a0aa8225b1de12f
Juho Holmi <juhoholmi@gmail.com>
2020-06-30 21:06:03 +0300

    1.7: unicafe step2 & 1.8: unicafe step3 done

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom'
 
 const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
 
+const Statistics = ({good, neutral, bad}) => {
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <p style={{color: "green"}}>Good: {good}</p>
+      <p style={{color: "blue"}}>Neutral: {neutral}</p>
+      <p style={{color: "red"}}>Bad: {bad}</p>
+      <br />
+      <p>Feedbacks given: {good + neutral + bad}</p>
+      <p>Average feedback score: {(good + neutral*0.0 + bad*-1) / (good + neutral + bad)}</p>
+      <p>Percentage of positive feedback: {(good) / (good + neutral + bad) * 100}%</p>
+    </div>
+  )
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
@@ -19,10 +34,7 @@ const App = () => {
       <Button handleClick={handleGood} text={"Good"}/>
       <Button handleClick={handleNeutral} text={"Neutral"}/>
       <Button handleClick={handleBad} text={"Bad"}/>
-      <h2>Statistics</h2>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
