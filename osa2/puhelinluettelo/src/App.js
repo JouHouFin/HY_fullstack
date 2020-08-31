ab2e869823cf74d9be6c91c807f7dd8ad042a0b7
Juho Holmi <juhoholmi@gmail.com>
2020-08-31 15:37:01 +0300

    2.15: puhelinluettelo step7 done

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -59,7 +59,8 @@ const App = () => {
 
   useEffect(hook, [])
   console.log("persons length: ", persons.length)
-  const addContact = (event) => {
+
+  const addContact = event => {
     event.preventDefault()
     if (newName === '') {
       alert(`Name cannot be empty`)
@@ -68,9 +69,17 @@ const App = () => {
     } else if (persons.some(person => person.name === newName)) {
       alert(`${newName} is already added to phonebook`)
     } else {
-      setPersons(persons.concat({name: newName, number: newNumber}))
-      setNewName('')
-      setNewNumber('')
+      const newPersonObject = {
+        name: newName, 
+        number: newNumber
+      }
+      
+      axios.post('http://localhost:3001/persons', newPersonObject)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+        setNewName('')
+        setNewNumber('')
+      })
     }
   }
 
