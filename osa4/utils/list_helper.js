70300edd3719456ad433da5cc17c24b43fe7a061
Juho Holmi <juhoholmi@gmail.com>
2020-09-18 12:59:08 +0300

    4.4: apufunktioita ja yksikkötestejä, step2 done

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.length === 0 ? 0 : blogs.reduce((acc, curr) => acc + curr.likes, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
