6e9d879f1c9dbb6dd5e79cb430a8aa4551f6157e
Juho Holmi <juhoholmi@gmail.com>
2020-09-18 13:38:58 +0300

    4.5*: apufunktioita ja yksikkötestejä, step3 done

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -6,7 +6,24 @@ const totalLikes = (blogs) => {
   return blogs.length === 0 ? 0 : blogs.reduce((acc, curr) => acc + curr.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+
+  const maxLikes = blogs.reduce((acc, curr) => acc = curr.likes > acc ? curr.likes : acc, 0)
+  const favBlog = blogs.find(blog => blog.likes === maxLikes)
+  return (
+    {
+      title: favBlog.title,
+      author: favBlog.author,
+      likes: favBlog.likes
+    }
+  )
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
