77da0e10ac4e318bdf17861d50c095a5bd83a26a
Juho Holmi <juhoholmi@gmail.com>
2020-12-09 12:03:49 +0200

    6.5*: anekdootit, step3 done

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -4,6 +4,7 @@ import { vote, create } from './reducers/anecdoteReducer'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
+  const sortedAnecdotes = anecdotes.sort((a, b) => b.votes - a.votes)
   const dispatch = useDispatch()
 
   const dispatchVote = (id) => {
@@ -19,7 +20,7 @@ const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {sortedAnecdotes.map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
