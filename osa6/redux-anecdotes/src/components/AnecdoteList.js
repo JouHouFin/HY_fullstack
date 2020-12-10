463118221d11abc0d4b65090f6a96e5a3c988d7c
Juho Holmi <juhoholmi@gmail.com>
2020-12-10 13:40:57 +0200

    6.12* paremmat anekdootit, step10 done

--- osa6/redux-anecdotes/src/components/AnecdoteList.js
+++ osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -5,8 +5,12 @@ import { voted, flush } from '../reducers/notificationReducer'
 
 export const AnecdoteList = () => {
   const anecdotes = useSelector(state => state.anecdotes)
-  const sortedAnecdotes = anecdotes.sort((a, b) => b.votes - a.votes)
+  const filter = useSelector(state => state.filter)
   const dispatch = useDispatch()
+
+  const regex = RegExp(filter, 'i');
+  const filteredAnecdotes = anecdotes.filter(anecdote => regex.test(anecdote.content))
+  const sortedAnecdotes = filteredAnecdotes.sort((a, b) => b.votes - a.votes)
 
   const dispatchVote = (anecdote) => {
     dispatch(vote(anecdote.id))
@@ -24,7 +28,7 @@ export const AnecdoteList = () => {
         </div>
         <div>
           has {anecdote.votes}
-          <button onClick={() => dispatchVote(anecdote)}>vote</button>
+          <button style={{ marginLeft: 5 }} onClick={() => dispatchVote(anecdote)}>vote</button>
         </div>
       </div>
     )
