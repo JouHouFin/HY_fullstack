2ccab348e9c11dd83e1dfb3bee0b485246937899
Juho Holmi <juhoholmi@gmail.com>
2020-12-14 13:05:04 +0200

    Further division to components

---
+++ osa7/routed-anecdotes/src/components/Menu.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+
+export const Menu = () => {
+  const padding = {
+    paddingRight: 5
+  }
+  return (
+    <div>
+      <Link to='/' style={padding}>Anecdotes</Link>
+      <Link to='/create' style={padding}>Create new</Link>
+      <Link to='/about' style={padding}>About</Link>
+    </div>
+  )
+}
